7394a0bd87173e6aff3c54d5d1f2363469f8f9d7
Nikolaev3Artem <artnik349@gmail.com>
2023-07-29 02:43:03 +0300

    added css minification

--- gulp/tasks/build/css.js
+++ gulp/tasks/build/css.js
@@ -1,4 +1,7 @@
+import cleanCss from "gulp-minify-css";
+
 export const css = () => {
-    return app.gulp.src(`${app.path.src.files}/${app.path.src.assets}/${app.path.src.css}/**`)
-     .pipe(app.gulp.dest(`${app.path.build.files}/${app.path.build.css}/`));
+    return app.gulp.src(`${app.path.src.files}/${app.path.src.assets}/${app.path.src.css}/*.css`)
+        .pipe(cleanCss())
+        .pipe(app.gulp.dest(`${app.path.build.files}/${app.path.build.css}/`));
  }
