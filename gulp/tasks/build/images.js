5f97e90f2b529a42849d8a9b7c16da90cce90b0b
Nikolaev3Artem <artnik349@gmail.com>
2023-08-08 23:12:15 +0300

    added image minification

--- gulp/tasks/build/images.js
+++ gulp/tasks/build/images.js
@@ -1,4 +1,7 @@
+import imagemin from 'gulp-imagemin';
+
 export const images = () => {
     return app.gulp.src(`${app.path.src.files}/${app.path.src.assets}/${app.path.src.images}/**`)
+     .pipe(imagemin())
      .pipe(app.gulp.dest(`${app.path.build.files}/${app.path.build.images}/`));
  }
